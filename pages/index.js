99989f6d5f0cc6978e4ce1b27d00119d557b642e
prateekpranveer <prateekpranveer321@gmail.com>
2025-08-03 20:44:45 +0530

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { client } from "@/src/sanity/lib/client";
-
+import { FiAperture } from "react-icons/fi";
 const totalGoal = 1000;
 
 export default function LiveTextEditor() {
@@ -131,9 +131,9 @@ export default function LiveTextEditor() {
       {/* Mobile toggle button moved to top-right */}
       <button
         onClick={() => setShowSidebar(!showSidebar)}
-        className="absolute top-4 right-4 z-50 md:hidden bg-gray-700 text-white px-3 py-2 rounded transition-transform duration-300 hover:scale-105"
+        className="absolute top-4 right-4 z-50 md:hidden px-3 py-2 rounded transition-transform duration-300 hover:scale-105"
       >
-        ☰
+        <FiAperture />
       </button>
 
       {/* Sidebar */}
